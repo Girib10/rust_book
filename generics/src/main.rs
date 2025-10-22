6ef9034fa09cf50b6a5d94912ec806d9cfe694a0
Girib10 <giribanhaia@gmail.com>
2025-10-22 15:09:25 -0300

    generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,38 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl<T> Point<T, f64> {
+    fn y(&self) -> &f64 {
+        &self.y
+    }
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p1 = Point {x:5, y: 7};
+    let p2 = Point {x:5.0, y: 7.0};
+    let p3 = Point {x:5, y: 7.0};
+
+    p3.y();
+
+    let number_list = vec![1, 5, 7, 8, 13];
+    println!("Largest in {:?} is {}", number_list, get_largest(&number_list))
+}
+
+fn get_largest<T: PartialOrd + Copy>(list: &Vec<T>) -> T {
+    let mut largest = list[0];
+    for value in list {
+        if *value > largest {
+            largest = *value;
+        }
+    }
+    largest
 }
